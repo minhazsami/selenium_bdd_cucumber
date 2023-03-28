0fc802f53fcb5a92a55d558503797029aa05d9e6
minhazsami <minhazsami@yahoo.com>
2023-03-28 12:47:53 +0600

    added the sector and pass data

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -1,2 +1,55 @@
-package stepDefinitions;public class Contact_Us_Steps {
+package stepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Contact_Us_Steps {
+    private WebDriver driver;
+    @Before
+    public void setup (){
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/divers/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+    }
+    @Given("I access the webdriver university contact us page")
+    public void i_access_the_webdriver_university_contact_us_page() {
+        driver.get("https://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    }
+    @When("I enter a unique first name")
+    public void i_enter_a_unique_first_name() {
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys("Mohammed");
+    }
+    @And("I enter a unique last name")
+    public void i_enter_a_unique_last_name() {
+        driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys("Minhaz");
+    }
+    @And("I enter a unique email address")
+    public void i_enter_a_unique_email_address() {
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("minhaz@gmail.com");
+    }
+    @And("I enter a unique comment")
+    public void i_enter_a_unique_comment() {
+        driver.findElement(By.xpath("//textarea[@name='message']")).sendKeys("This is a text command");
+    }
+    @And("I click the submit button")
+    public void i_click_the_submit_button() {
+        driver.findElement(By.xpath("//input[@value='SUBMIT']")).click();
+    }
+    @Then("I should be presented with a successful contact us submission message")
+    public void i_should_be_presented_with_a_successful_contact_us_submission_message() {
+
+        System.out.println("Test7");
+    }
 }
