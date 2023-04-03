e5932e4477e482344d4d2ccdd00531c9b9a905a0
minhazsami <minhazsami@yahoo.com>
2023-04-03 09:43:34 +0600

    Completed lecture no 30

--- src/test/java/stepDefinitions/Login.java
+++ src/test/java/stepDefinitions/Login.java
@@ -1,11 +1,31 @@
 package stepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Login {
+    private WebDriver driver;
+    @Before("@login")
+    public void setup (){
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/divers/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+    }
+    @After("@login")
+    public void teardown(){
+        driver.quit();
+    }
     @Given("I have to access the login page")
     public void i_have_to_access_the_login_page() {
 
